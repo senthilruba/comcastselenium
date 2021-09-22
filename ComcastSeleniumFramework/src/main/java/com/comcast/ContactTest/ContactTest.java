7d4da712abf5b9b6953e0bd0d45d9fb0684fd70b
senthilruba <senthilruba15@gmail.com>
2021-09-22 12:19:22 +0530

    new one

---
+++ ComcastSeleniumFramework/src/main/java/com/comcast/ContactTest/ContactTest.java
@@ -0,0 +1,21 @@
+package com.comcast.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+		@Test
+		public void creaateContactTest() {
+			System.out.println("execute createContactTest");
+		}
+
+		public void creaateContactWithOrgTest() {
+			System.out.println("execute createContactWithOrgTest");
+		}
+		
+		public void deleteContactTest() {
+			System.out.println("execute deleteContactTest");
+		}
+		
+	}
+
+
